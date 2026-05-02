dc547cace59160e66b7d760e8dbd365f62a341f9
XiaoDong <mxd2166846893@163.com>
2026-05-02 23:17:47 +0800

    Fix table context menu interactions

--- MaDB.Desktop/Views/MainWindow.axaml.cs
+++ MaDB.Desktop/Views/MainWindow.axaml.cs
@@ -1,6 +1,6 @@
 using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.VisualTree;
+using Avalonia.Input.Platform;
 using MaDB.Desktop.ViewModels;
 
 namespace MaDB.Desktop.Views;
@@ -34,7 +34,7 @@ public partial class MainWindow : Window
     private void OnViewTableDataClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
         if (sender is MenuItem menuItem &&
-            menuItem.DataContext is DatabaseTableViewModel table &&
+            TryGetTable(menuItem) is { } table &&
             DataContext is MainWindowViewModel vm)
         {
             vm.OpenTableTabCommand.Execute(table.Name);
@@ -44,25 +44,31 @@ public partial class MainWindow : Window
     private void OnViewTableSchemaClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
         if (sender is MenuItem menuItem &&
-            menuItem.DataContext is DatabaseTableViewModel table &&
+            TryGetTable(menuItem) is { } table &&
             DataContext is MainWindowViewModel vm)
         {
             vm.OpenTableSchemaCommand.Execute(table.Name);
         }
     }
 
-    private void OnCopyTableNameClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    private async void OnCopyTableNameClick(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        // Clipboard API varies by Avalonia version
-        // if (sender is MenuItem menuItem &&
-        //     menuItem.DataContext is DatabaseTableViewModel table)
-        // {
-        //     var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
-        //     if (clipboard is not null)
-        //     {
-        //         _ = clipboard.SetTextAsync(table.Name);
-        //     }
-        // }
+        if (sender is not MenuItem menuItem || TryGetTable(menuItem) is not { } table)
+        {
+            return;
+        }
+
+        var clipboard = TopLevel.GetTopLevel(this)?.Clipboard;
+        if (clipboard is not null)
+        {
+            await clipboard.SetTextAsync(table.Name);
+        }
+    }
+
+    private static DatabaseTableViewModel? TryGetTable(MenuItem menuItem)
+    {
+        return menuItem.Tag as DatabaseTableViewModel
+            ?? menuItem.DataContext as DatabaseTableViewModel;
     }
 
     private void OnTabTapped(object? sender, TappedEventArgs e)
