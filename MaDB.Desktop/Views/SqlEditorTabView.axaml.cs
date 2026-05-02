3b0d137bed4b0c54cf4440474b80ee29afa8bff9
XiaoDong <mxd2166846893@163.com>
2026-05-02 19:40:15 +0800

    Add SQL editor syntax highlighting

--- MaDB.Desktop/Views/SqlEditorTabView.axaml.cs
+++ MaDB.Desktop/Views/SqlEditorTabView.axaml.cs
@@ -1,17 +1,31 @@
+using System;
 using System.ComponentModel;
 using Avalonia.Controls;
+using AvaloniaEdit.TextMate;
 using MaDB.Desktop.ViewModels;
+using TextMateSharp.Grammars;
 
 namespace MaDB.Desktop.Views;
 
 public partial class SqlEditorTabView : UserControl
 {
     private SqlEditorTabViewModel? _viewModel;
+    private bool _isSyncingText;
+    private TextMate.Installation? _textMateInstallation;
 
     public SqlEditorTabView()
     {
         InitializeComponent();
+        InitializeSqlEditor();
         DataContextChanged += (_, _) => SetViewModel(DataContext as SqlEditorTabViewModel);
+    }
+
+    private void InitializeSqlEditor()
+    {
+        var registryOptions = new RegistryOptions(ThemeName.DarkPlus);
+        _textMateInstallation = SqlTextEditor.InstallTextMate(registryOptions);
+        _textMateInstallation.SetGrammar(registryOptions.GetScopeByLanguageId("sql"));
+        SqlTextEditor.TextChanged += OnSqlEditorTextChanged;
     }
 
     private void SetViewModel(SqlEditorTabViewModel? viewModel)
@@ -30,6 +44,7 @@ public partial class SqlEditorTabView : UserControl
         }
 
         _viewModel.PropertyChanged += OnViewModelPropertyChanged;
+        SetEditorText(_viewModel.SqlText);
         QueryResultGridColumns.Rebuild(ResultGrid, _viewModel.ResultColumnNames);
     }
 
@@ -39,5 +54,34 @@ public partial class SqlEditorTabView : UserControl
         {
             QueryResultGridColumns.Rebuild(ResultGrid, _viewModel.ResultColumnNames);
         }
+
+        if (e.PropertyName == nameof(SqlEditorTabViewModel.SqlText) && _viewModel is not null)
+        {
+            SetEditorText(_viewModel.SqlText);
+        }
+    }
+
+    private void OnSqlEditorTextChanged(object? sender, EventArgs e)
+    {
+        if (_isSyncingText || _viewModel is null)
+        {
+            return;
+        }
+
+        _isSyncingText = true;
+        _viewModel.SqlText = SqlTextEditor.Text;
+        _isSyncingText = false;
+    }
+
+    private void SetEditorText(string text)
+    {
+        if (_isSyncingText || SqlTextEditor.Text == text)
+        {
+            return;
+        }
+
+        _isSyncingText = true;
+        SqlTextEditor.Text = text;
+        _isSyncingText = false;
     }
 }
